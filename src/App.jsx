4b2d17f8a7c6296f58d594b234c122160de11fc1
Koji-Sunioj <koji.gabriel218@gmail.com>
2026-01-29 17:30:52 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">testing cloudfront</p>
+    </>
+  );
+}
+
+export default App;
