1c334f169c6c57a2f7e4cff299ed4abcab6c0094
Koji-Sunioj <koji.gabriel218@gmail.com>
2026-01-29 19:22:15 +0200

    testing website

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <p className="read-the-docs">testing cloudfront</p>
+      <p className="read-the-docs">
+        testing cloudfront: {import.meta.env.VITE_TITLE}
+      </p>
     </>
   );
 }
